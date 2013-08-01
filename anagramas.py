d302dbeb6517f412d633d1b61456dcb97b243c58
Guilherme Mangabeira Gregio <guilherme@3gweb.com.br>
2013-08-01 19:02:23 -0300

    adicionado test para anagrama de 3 letras com loop for statico

--- anagramas.py
+++ anagramas.py
@@ -2,12 +2,22 @@ class Anagramas:
 
 	def __init__(self, word):
 		self.word = word
-		self.result = [word]
+		self.result = []
 
-	def generate(self):		
-		if(len(self.word) > 1):
+	def generate(self):
+
+		if(len(self.word) == 1):
+			self.result.append(self.word)
+
+		if(len(self.word) == 2):
+			self.result.append(self.word)
 			self.result.append(self.word[::-1])
 
+		if(len(self.word) == 3):
+			for i in range(0, len(self.word)):
+				for ii in range(0, len(self.word)):
+					for iii in range(0, len(self.word)):
+						if i != ii and ii != iii and i != iii:
+							self.result.append(self.word[i] + self.word[ii] + self.word[iii])
+
 		return self.result
-
-
--- anagramasTest.py
+++ anagramasTest.py
@@ -12,3 +12,13 @@ class AnagramasTest(TestCase):
 		anagrama = Anagramas('ab')
 		
 		self.assertEqual(['ab', 'ba'], anagrama.generate())
+
+	def testShouldAnagramThenWordTwoLettersInverse(self):
+		anagrama = Anagramas('ba')
+		
+		self.assertEqual(['ba', 'ab'], anagrama.generate())
+
+	def testShouldAnagramThenWordThreeLetters(self):
+		anagrama = Anagramas('abc')
+
+		self.assertEqual(['abc', 'acb', 'bac', 'bca', 'cab', 'cba'], anagrama.generate())
