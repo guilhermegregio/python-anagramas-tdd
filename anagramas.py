b601690c3eb6d13dabcea6fcd2f99b7ecfa866e4
Guilherme Mangabeira Gregio <guilherme@3gweb.com.br>
2013-08-02 14:20:45 -0300

    corrigindo identacao

--- anagramas.py
+++ anagramas.py
@@ -1,12 +1,12 @@
 
 def generate(word):
-		result = set()
+	result = set()
 
-		if word == "":
-			return [word]
-		else:
-			for w in generate(word[1:]):
-				for pos in range(len(w)+1):
-					result.add(w[:pos]+word[0]+w[pos:])
-			
-			return list(result)
+	if word == "":
+		return [word]
+	else:
+		for w in generate(word[1:]):
+			for pos in range(len(w)+1):
+				result.add(w[:pos]+word[0]+w[pos:])
+		
+		return list(result)
